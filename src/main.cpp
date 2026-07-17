0e021c43ddd96d2e6ff3dbc54ac6eeb1759e2dce
KristianHooymans <hooymanskristian@gmail.com>
2026-07-17 11:50:47 +1000

    fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,13 @@ int main(int argc, char* argv[])
 {
   if (argc == 2) {
     std::vector<uint8_t> rom = loadRom(argv[1]);
-    int countLines = 0; // what line we are on in the loop.
     int offset = 0; // for the loop, basically what line we are on right now in the ROM
     for (auto i {0uz}; i < 20; ++i) {
-      std::cout << std::hex << std::setfill('0') << std::setw(7) << countLines << '0' << ": ";
-     for (auto i {0uz}; i < 16; ++i) {
-        std::cout << std::hex << std::setfill('0') << std::setw(2) <<  static_cast<int>(rom[i+offset]) << " ";
+      std::cout << std::hex << std::setfill('0') << std::setw(7) << i << '0' << ": ";
+     for (auto i {0uz}; j < 16; ++j) {
+        std::cout << std::hex << std::setfill('0') << std::setw(2) <<  static_cast<int>(rom[j+offset]) << " ";
       }
      std::cout << '\n';
-     countLines++;
      offset += 16;
     }
     std::cout << std::dec;
